fa4ed1f578a83be757332dd029e1ce6d2e98ae59
Osama <osam2yach@gmail.com>
2026-09-03 13:09:04 +0300

    refactor(mobile): centralize shared account rules

---
+++ lib/accounts/account-rules.ts
@@ -0,0 +1,10 @@
+const RESTRICTED_ACCOUNT_STATUSES = new Set([
+  "suspended",
+  "blocked",
+  "banned",
+  "disabled",
+]);
+
+export function isRestrictedAccountStatus(status: string | null | undefined) {
+  return RESTRICTED_ACCOUNT_STATUSES.has(status ?? "");
+}
