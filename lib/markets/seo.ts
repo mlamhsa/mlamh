1d14541517e788b5bc2b4bce04e7cfa701c4369e
Osama <osam2yach@gmail.com>
2026-09-03 08:29:57 +0300

    refactor(markets): derive SEO markets from country registry

--- lib/markets/seo.ts
+++ lib/markets/seo.ts
@@ -1,11 +1,10 @@
 import { isMarketFeatureEnabled } from "./config.ts";
-import type { CountryCode } from "./countries.ts";
+import { COUNTRY_CODES, type CountryCode } from "./countries.ts";
 
 export function canIndexMarket(countryCode: CountryCode): boolean {
   return isMarketFeatureEnabled(countryCode, "seoIndexing");
 }
 
 export function getIndexableMarkets(): CountryCode[] {
-  const markets: CountryCode[] = ["SA", "AE", "EG", "MA", "QA", "JO", "LB", "KW"];
-  return markets.filter(canIndexMarket);
+  return COUNTRY_CODES.filter(canIndexMarket);
 }
