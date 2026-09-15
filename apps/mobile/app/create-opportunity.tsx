e1e2779fc5016d11295d33f5b6944ad028e2ba22
Osama <osam2yach@gmail.com>
2026-09-15 12:32:03 +0300

    feat(mobile): add publisher create opportunity route

---
+++ apps/mobile/app/create-opportunity.tsx
@@ -0,0 +1,5 @@
+import { PublisherCreateOpportunityScreen } from "@/src/domains/publisher/PublisherCreateOpportunityScreen";
+
+export default function CreateOpportunityRoute() {
+  return <PublisherCreateOpportunityScreen />;
+}
