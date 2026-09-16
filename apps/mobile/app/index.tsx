c41267a99cb17b5e58dc815255a26e7155629730
Osama <osam2yach@gmail.com>
2026-09-16 13:10:39 +0300

    test(mobile): add root launch smoke marker

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
+
 import { RootRouteGate } from "@/src/navigation/RootRouteGate";
 
 export default function IndexScreen() {
+  useEffect(() => {
+    console.log("[MLAMH_LAUNCH] root-route-mounted");
+  }, []);
+
   return <RootRouteGate />;
 }
